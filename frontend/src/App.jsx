746f56b16232a9cdb5030341a1055230cf1bd537
Pedro Maia <pedrovcsmaia@gmail.com>
2026-03-12 15:38:55 +0000

    chore: add docker dev setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
       <div className="flex items-center justify-center h-full text-white text-2xl">
-        Periodic Table of Languages
+        Periodic Table of Languages!!
       </div>
   )
 }
